a2dbf25d77ade8b1140339ec7eda10dbaff72d40
Mattia <mattia.tummolilllo@gmail.com>
2020-10-20 16:00:48 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -19,12 +19,12 @@ var prezzoBiglietto = (0.21 * numeroKm).toFixed(2);
 // SCONTI (con approssimazione a due decimali)
 if (numeroEta < 18) { // sconto under 18
    sconto = (prezzoBiglietto / 100) * 20; //sconto minorenni del 20%
-   prezzoFinale = (prezzoBiglietto - sconto).toFixed(2); //approssimazione
+   prezzoFinale = (prezzoBiglietto - sconto).toFixed(2) + (" (-20%)"); //approssimazione
    // console.log(("Valore sconto: " + sconto));
    // console.log("Prezzo con sconto del 20% : " + prezzoFinale);
 } else if (numeroEta >= 65){  //sconto over 65
    sconto = (prezzoBiglietto / 100) * 40; //sconto over 65 del 40%
-   prezzoFinale = (prezzoBiglietto - sconto).toFixed(2);; //approssimazione
+   prezzoFinale = (prezzoBiglietto - sconto).toFixed(2) + (" (-40%)"); //approssimazione
    // console.log(("Valore sconto: " + sconto));
    // console.log(("Prezzo con sconto del 40% : ") + prezzoFinale);
 } else {
